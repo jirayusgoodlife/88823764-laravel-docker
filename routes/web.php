8c43d32a02dba4f7ec8423dc7c8c0df777f96b1e
bankkamo926 <bankkamo926@gmail.com>
2023-03-23 19:43:14 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('/login', [App\Http\Controllers\loginController::class, 'login'])->name('login');
